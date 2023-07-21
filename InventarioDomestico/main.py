0258a146fb113b4daaf0e7a7e8684f5ddc354891
diegofurukawa <53219860+diegofurukawa@users.noreply.github.com>
2023-07-21 12:23:15 -0300

    update
    update

--- InventarioDomestico/main.py
+++ InventarioDomestico/main.py
@@ -14,9 +14,8 @@ from datetime import date
 co0 = "#000000" #Black
 co1 = "#feffff" #Branca
 co2 = "#4fa882" #Green
-
 co3 = "#000000" #Preta
-co4 = "#000000" #Preta
+co4 = "#403d3d" #Preta
 co5 = "#000000" #Preta
 co6 = "#000000" #Preta
 co7 = "#000000" #Preta
@@ -25,6 +24,7 @@ co9 = "#F8F8FF" #GhostWhite
 co10 = "#696969" #DimGray
  
 
+#Criando Janela
 
 window = Tk()
 window.title('')
@@ -37,63 +37,63 @@ style.theme_use("clam")
 
 
 #Criando Frames
-frameHeader = Frame(window, width=899, height=50, bg=co1, relief=FLAT)
-frameHeader.grid(row=0, column=0)
+frameCima = Frame(window, width=899, height=50, bg=co1, relief=FLAT)
+frameCima.grid(row=0, column=0)
 
-frameCenter = Frame(window, width=899, height=303, bg=co1, pady=20, relief=FLAT)
-frameCenter.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
+frameMeio = Frame(window, width=899, height=303, bg=co1, pady=20, relief=FLAT)
+frameMeio.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
 
-frameTop = Frame(window, width=899, height=300, bg=co1, pady=20, relief=FLAT)
-frameTop.grid(row=2, column=0, pady=0, padx=1, sticky=NSEW)
+frameBaixo = Frame(window, width=899, height=300, bg=co1, pady=20, relief=FLAT)
+frameBaixo.grid(row=2, column=0, pady=0, padx=1, sticky=NSEW)
 
 #Abrindo Imagem
 app_img = Image.open('icons/icon_customer.png')
 app_img = app_img.resize((45,45))
 app_img = ImageTk.PhotoImage(app_img)
 
-app_logo = Label(frameHeader, image=app_img, text=" Customer Registration", width=900, compound=LEFT, relief=RAISED, anchor=NW, font=('Verdana 20 bold'), bg=co1, fg=co4)
+app_logo = Label(frameCima, image=app_img, text=" Inventario Domestico", width=900, compound=LEFT, relief=RAISED, anchor=NW, font=('Verdana 20 bold'), bg=co1, fg=co4)
 app_logo.place(x=0, y=0)
 
 
 #Criando Labels
 
-l_name = Label(frameCenter, text='Name Customer', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_name = Label(frameMeio, text='Name Customer', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_name.place(x=10,y=10)
 
-e_name = Entry(frameCenter, width=50, justify='left', relief=SOLID)
+e_name = Entry(frameMeio, width=50, justify='left', relief=SOLID)
 e_name.place(x=130,y=11)
 
 # ====================================================================================================================================
 
-l_salesman = Label(frameCenter, text='Sales', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_salesman = Label(frameMeio, text='Sales', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_salesman.place(x=10,y=40)
 
-e_salesman = Entry(frameCenter, width=50, justify='left', relief=SOLID)
+e_salesman = Entry(frameMeio, width=50, justify='left', relief=SOLID)
 e_salesman.place(x=130,y=41)
 
 # ====================================================================================================================================
 
-l_phonenumber = Label(frameCenter, text='Phone Number', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_phonenumber = Label(frameMeio, text='Phone Number', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_phonenumber.place(x=10,y=70)
 
-e_phonenumber = Entry(frameCenter, width=50, justify='left', relief=SOLID)
+e_phonenumber = Entry(frameMeio, width=50, justify='left', relief=SOLID)
 e_phonenumber.place(x=130,y=71)
 
 
 # ====================================================================================================================================
 
-l_description = Label(frameCenter, text='Description', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_description = Label(frameMeio, text='Description', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_description.place(x=10,y=100)
 
-e_description = Entry(frameCenter, width=50, justify='left', relief=SOLID)
+e_description = Entry(frameMeio, width=50, justify='left', relief=SOLID)
 e_description.place(x=130,y=101)
 
 # ====================================================================================================================================
 
-l_startcontract = Label(frameCenter, text='Start Contract', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
+l_startcontract = Label(frameMeio, text='Start Contract', height=1, anchor=NW, font=('Ivy 10 bold'), bg=co1, fg=co4)
 l_startcontract.place(x=10,y=130)
 
-e_startcontract = DateEntry(frameCenter, width=12, Background='darkblue', bordewidth=2, year=2023)
+e_startcontract = DateEntry(frameMeio, width=12, Background='darkblue', bordewidth=2, year=2023)
 e_startcontract.place(x=130,y=131)
 
 
@@ -103,7 +103,7 @@ img_add = Image.open('icons/icons8-add-48.png')
 img_add = img_add.resize((17,17))
 img_add = ImageTk.PhotoImage(img_add)
 
-b_insert = Button(frameCenter, image=img_add, width=95, text=' Adicionar'.upper(), compound=LEFT, anchor=NW, overrelief=RIDGE, font='Ivy 8',  bg=co1, fg=co0)
+b_insert = Button(frameMeio, image=img_add, width=95, text=' Adicionar'.upper(), compound=LEFT, anchor=NW, overrelief=RIDGE, font='Ivy 8',  bg=co1, fg=co0)
 b_insert.place(x=450, y=8)
 
 
@@ -113,7 +113,7 @@ img_ref = Image.open('icons/icons8-refresh-48.png')
 img_ref = img_ref.resize((17,17))
 img_ref = ImageTk.PhotoImage(img_ref)
 
-b_insert = Button(frameCenter, image=img_ref, width=95, text=' Atualizar'.upper(), compound=LEFT, anchor=NW, overrelief=RIDGE, font='Ivy 8',  bg=co1, fg=co0)
+b_insert = Button(frameMeio, image=img_ref, width=95, text=' Atualizar'.upper(), compound=LEFT, anchor=NW, overrelief=RIDGE, font='Ivy 8',  bg=co1, fg=co0)
 b_insert.place(x=450, y=38)
 
 
@@ -123,7 +123,7 @@ img_del = Image.open('icons/icons8-delete-100.png')
 img_del = img_del.resize((17,17))
 img_del = ImageTk.PhotoImage(img_del)
 
-b_insert = Button(frameCenter, image=img_del, width=95, text=' Excluir'.upper(), compound=LEFT, anchor=NW, overrelief=RIDGE, font='Ivy 8',  bg=co1, fg=co0)
+b_insert = Button(frameMeio, image=img_del, width=95, text=' Excluir'.upper(), compound=LEFT, anchor=NW, overrelief=RIDGE, font='Ivy 8',  bg=co1, fg=co0)
 b_insert.place(x=450, y=70)
 
 window.mainloop()
